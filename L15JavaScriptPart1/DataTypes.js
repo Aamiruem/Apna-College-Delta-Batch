7f9eeccfc9f071282faaf2ee1081ade80f95835b
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-01-14 22:46:31 +0530

    commit add

---
+++ L15JavaScriptPart1/DataTypes.js
@@ -0,0 +1,33 @@
+// primitive types
+
+// number=> positive or negative numbers 
+let a = 16;
+let b = 6;
+let c = a + b;
+console.log(c);
+console.log(typeof(c));
+
+
+// string
+// boolean
+// undefined
+// null
+// symbol
+// bigint
+//symbol
+// object
+//
+// function
+
+
+
+
+// Data Types in JS
+// Primitive Types
+// • Number
+// • Boolean
+// • String
+// • Undefined
+// • Null
+// • Bigint
+// • Symbol
---
+++ L15JavaScriptPart1/UnaryOperators.js
@@ -0,0 +1,50 @@
+// unary operators
+// +, -, !, typeof, void, delete, ++, --, ~
+
+
+let y = 2000;
+
+let z = -y; 
+
+
+let x1 = "10";
+x = +x1; // x is now 10 (number)
+console.log(x)
+
+let x2 = "10";
+x = -x; // x is now -10 (number)
+console.log(x)
+
+//pre increment
+let x3 = 5;
+let y1 = ++x; // y is 6, x is 6 (prefix)
+console.log(y1);
+
+//post increment
+x = 5;
+y = x++; // y is 5, x is 6 (postfix)
+console.log(y);
+
+
+//unary negation
+let x4 = true;
+let y2 = !x4; // y is false
+console.log(y2);
+
+
+//typeof operator
+let x = 5;
+void x; // evaluates x, but the result is always undefined
+console.log(x);
+
+
+//
+let obj = {a: 1, b: 2};
+delete obj.a; // returns true, obj is now {b: 2}
+console.log(obj);
+
+
+
+//typeof operator
+let xs = 5;
+typeof xs; // returns "number"   
