5570a9e24467c883935314df409062a6700ddb16
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-03-06 21:45:40 +0530

    commit add

--- L22javascriptPart8/spread.js
+++ L22javascriptPart8/spread.js
@@ -1,3 +1,16 @@
+// spread operator in JavaScript is a powerful syntax that allows you to expand elements of an iterable (like an array or object) into individual elements. It is denoted by three dots (...). The spread operator can be used in various contexts, such as function calls, array literals, and object literals.It is often used to find the minimum or maximum value in an array, combine arrays, or merge objects.
+
+//  🔹 Example: Finding Minimum and Maximum Values in an Array
+// javascript
+
+let numbers = [5, 2, 9, 1, 5, 6];
+
+let mins = Math.min(...numbers);
+console.log("Minimum value:", mins); // Output: Minimum value: 1
+
+let max = Math.max(...numbers);
+console.log("Maximum value:", max); // Output: Maximum value: 9
+
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // let min = Math.min(arr);
