3e3783e8af0ad2bdb82efef8d319c044769a6dc0
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-04-16 18:00:02 +0530

    COMMIT ADD

--- L43BackendRest/Next.js
+++ L43BackendRest/Next.js
@@ -1,129 +1,219 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const bodyParser = require('body-parser');
+
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
+
+// // Set views directory and view engine
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// // Use body-parser middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // Route for home page
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+
+// // Route for submitting form data
+// app.post('/submit', (req, res) => {
+//     const { username, content } = req.body;
+    
+//     // Process data here if needed
+    
+//     // Redirect to new page or render new template
+//     res.redirect('/new'); // or res.render('new', { username, content });
+// });
+
+// // Route for new page
+// app.get('/new', (req, res) => {
+//     res.render('new');
+// });
+
+
+// app.get('/post/:id', (req, res) => {
+//     const post = {
+//         id: req.params.id,
+//         username: 'user123',
+//         content: 'This is the post content'
+//     };
+//     res.render('post-details', { post }); // Renders the post-details.ejs view and passes the post object
+// });
+
+
+
+// // Route to handle the post creation
+// app.post('/posts', (req, res) => {
+//     const { username, content } = req.body; // Extract form data
+//     // You can now save `username` and `content` to the database or any storage
+//     console.log('Username:', username);
+//     console.log('Post Content:', content);
+
+//     // Respond or redirect after handling the form
+//     res.redirect('/'); // Or any other page you'd like to redirect to
+// });
+
+// app.listen(8080, () => {
+//     console.log('Server running on port 3000');
+// });
+
+// app.get('/posts', (req, res) => {
+//     // Assuming you're using MySQL or any database, fetch the posts
+//     // For example, if using MySQL, you could do something like:
+//     const sql = 'SELECT * FROM posts';
+    
+//     db.query(sql, (err, results) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('Server Error');
+//         } else {
+//             // Pass the posts array to the EJS template
+//             res.render('all-posts', { posts: results });
+//         }
+//     });
+// });
+
+
+// app.delete('/posts/:id', (req, res) => {
+//     const postId = req.params.id;
+//     const sql = 'DELETE FROM posts WHERE id = ?';
+    
+//     db.query(sql, [postId], (err, result) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('Server Error');
+//         } else {
+//             res.redirect('/posts'); // Redirect back to all posts after deletion
+//         }
+//     });
+// });
+
+
+// app.get('/posts/:id/edit', (req, res) => {
+//     const postId = req.params.id;
+//     const sql = 'SELECT * FROM posts WHERE id = ?';
+
+//     db.query(sql, [postId], (err, result) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('Server Error');
+//         } else {
+//             res.render('edit-post', { post: result[0] });
+//         }
+//     });
+// });
+
+// app.put('/posts/:id', (req, res) => {
+//     const postId = req.params.id;
+//     const { username, content } = req.body;
+//     const sql = 'UPDATE posts SET username = ?, content = ? WHERE id = ?';
+
+//     db.query(sql, [username, content, postId], (err, result) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('Server Error');
+//         } else {
+//             res.redirect('/posts');
+//         }
+//     });
+// });
+
+
+// app.listen(8080, () => {
+//     console.log('Server is running on port 8080');
+// });
+
+
+
+
+
+// // nodemon Next.js run command interminal
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
-
 const methodOverride = require('method-override');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// Set views directory and view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use body-parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+let posts = [];
 
-// Route for home page
+// HOME
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/posts');
 });
 
-// Route for submitting form data
-app.post('/submit', (req, res) => {
-    const { username, content } = req.body;
-    
-    // Process data here if needed
-    
-    // Redirect to new page or render new template
-    res.redirect('/new'); // or res.render('new', { username, content });
+// INDEX
+app.get('/posts', (req, res) => {
+    res.render('all-posts', { posts });
 });
 
-// Route for new page
-app.get('/new', (req, res) => {
+// NEW
+app.get('/posts/new', (req, res) => {
     res.render('new');
 });
 
-
-app.get('/post/:id', (req, res) => {
-    const post = {
-        id: req.params.id,
-        username: 'user123',
-        content: 'This is the post content'
-    };
-    res.render('post-details', { post }); // Renders the post-details.ejs view and passes the post object
-});
-
-
-
-
-
-// Route to handle the post creation
+// CREATE
 app.post('/posts', (req, res) => {
-    const { username, content } = req.body; // Extract form data
-    // You can now save `username` and `content` to the database or any storage
-    console.log('Username:', username);
-    console.log('Post Content:', content);
-
-    // Respond or redirect after handling the form
-    res.redirect('/'); // Or any other page you'd like to redirect to
-});
-
-app.listen(8080, () => {
-    console.log('Server running on port 3000');
-});
-
-app.get('/posts', (req, res) => {
-    // Assuming you're using MySQL or any database, fetch the posts
-    // For example, if using MySQL, you could do something like:
-    const sql = 'SELECT * FROM posts';
-    
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-        } else {
-            // Pass the posts array to the EJS template
-            res.render('all-posts', { posts: results });
-        }
-    });
-});
-
-
-app.delete('/posts/:id', (req, res) => {
-    const postId = req.params.id;
-    const sql = 'DELETE FROM posts WHERE id = ?';
-    
-    db.query(sql, [postId], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-        } else {
-            res.redirect('/posts'); // Redirect back to all posts after deletion
-        }
-    });
-});
-
-
-app.get('/posts/:id/edit', (req, res) => {
-    const postId = req.params.id;
-    const sql = 'SELECT * FROM posts WHERE id = ?';
-
-    db.query(sql, [postId], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-        } else {
-            res.render('edit-post', { post: result[0] });
-        }
-    });
-});
-
-app.put('/posts/:id', (req, res) => {
-    const postId = req.params.id;
     const { username, content } = req.body;
-    const sql = 'UPDATE posts SET username = ?, content = ? WHERE id = ?';
 
-    db.query(sql, [username, content, postId], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-        } else {
-            res.redirect('/posts');
-        }
-    });
+    const newPost = {
+        id: Date.now().toString(),
+        username,
+        content
+    };
+
+    posts.push(newPost);
+    res.redirect('/posts');
 });
 
+// SHOW
+app.get('/posts/:id', (req, res) => {
+    const post = posts.find(p => p.id === req.params.id);
+    res.render('post-details', { post });
+});
+
+// EDIT
+app.get('/posts/:id/edit', (req, res) => {
+    const post = posts.find(p => p.id === req.params.id);
+    res.render('edit-post', { post });
+});
+
+// UPDATE
+app.put('/posts/:id', (req, res) => {
+    const post = posts.find(p => p.id === req.params.id);
+
+    if (post) {
+        post.username = req.body.username;
+        post.content = req.body.content;
+    }
+
+    res.redirect('/posts');
+});
+
+// DELETE
+app.delete('/posts/:id', (req, res) => {
+    posts = posts.filter(p => p.id !== req.params.id);
+    res.redirect('/posts');
+});
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+    console.log('Server running on port 8080');
 });
--- L43BackendRest/index.js
+++ L43BackendRest/index.js
@@ -1,14 +1,173 @@
+// const express = require("express");
+// const app = express();
+// const port = 8080;
+
+// const path = require("path");
+// const { v4: uuidv4 } = require('uuid');
+
+// const methodOverride = require("method-override");
+
+
+// app.use(express.json()); // for parsing application/json
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
+
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.use(express.static(path.join(__dirname, "public")));
+
+
+// let posts = [
+//     {
+//         id: uuidv4(),
+//         username: "apna college",
+//         content: "I love coding"
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "kamran",
+//         content: "Hard work is important to achieve a success! "
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "Aamir",
+//         content: "I love Data science"
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "Adam khan",
+//         content: "I got selected for first my internship "
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "apna college",
+//         content: "I love coding"
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "kamran",
+//         content: "Hard work is important to achieve a success! "
+//     },
+
+//     {
+//         id: uuidv4(),
+//         username: "Aamir",
+//         content: "I love Data science"
+//     },
+
+// ];
+
+
+// app.get("/posts", (req, res) => {
+//     res.render("index.ejs", { posts });
+//     // res.send("server working well!");
+// });
+
+
+// app.get("/posts/new", (req, res) => {
+//     res.render("new.ejs");
+// });
+
+// app.post("/posts", (req, res) => {
+//     console.log(req.body);
+//     res.send("post request working");
+
+//     let { username, content } = req.body;
+//     posts.push({ username, content });
+//     res.redirect("/posts");
+// });
+
+
+// app.post("/posts", (req, res) => {
+//     let { username, content } = req.body;
+//     let id = uuidv4();
+//     posts.push({ id, username, content });
+//     res.redirect("/posts");
+//     res.render("index.ejs", { posts });
+// });
+
+
+
+// app.get("/posts/:id", (req, res) => {
+//     let { id } = req.params;
+//     console.log(id);
+//     let post = posts.find((p) => id === p.id);
+//     res.render("show.ejs", { post });
+
+// });
+
+
+// app.patch("/posts/:id", (req, res) => {
+//     let { id } = req.params;
+//     let newContent = req.body.content;
+//     console.log(newContent)
+//     console.log(id)
+//     res.send("patch request is working ")
+//     let post = posts.find((p) => id === p.id);
+//     post.content = newContent;
+//     console.log(post);
+//     res.redirect("/posts");
+//     // res.render("show.ejs", { post });
+//     // res.redirect(`/posts/${id}`);
+
+// });
+
+
+// app.get("/posts/:id/edit", (req, res) => {
+//     let { id } = req.params;
+//     let post = posts.find((p) => id === p.id);
+//     res.render("edit.ejs", { post });
+// });
+
+// app.delete("/posts/:id", (req, res) => {
+//     let { id } = req.params;
+//     posts = posts.filter((p) => id !== p.id);
+//     res.render("show.ejs", { post });
+//     res.send("post deleted successfully");
+//     res.redirect("/posts");
+
+// });
+
+// // DELETE ROUTE
+// app.delete("/posts/:id", (req, res) => {
+//     const index = posts.findIndex(p => p.id == req.params.id);
+
+//     if (index !== -1) {
+//         posts.splice(index, 1);
+//         res.send("Post deleted");
+//     } else {
+//         res.status(404).send("Post not found");
+//     }
+// });
+
+
+
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
+
+
+
+
 const express = require("express");
 const app = express();
 const port = 8080;
 
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
 const methodOverride = require("method-override");
 
-
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
@@ -16,126 +175,92 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 let posts = [
-    {
-        id: uuidv4(),
-        username: "apna college",
-        content: "I love coding"
-    },
-
-    {
-        id: uuidv4(),
-        username: "kamran",
-        content: "Hard work is important to achieve a success! "
-    },
-
-    {
-        id: uuidv4(),
-        username: "Aamir",
-        content: "I love Data science"
-    },
-
-    {
-        id: uuidv4(),
-        username: "Adam khan",
-        content: "I got selected for first my internship "
-    },
-
-    {
-        id: uuidv4(),
-        username: "apna college",
-        content: "I love coding"
-    },
-
-    {
-        id: uuidv4(),
-        username: "kamran",
-        content: "Hard work is important to achieve a success! "
-    },
-
-    {
-        id: uuidv4(),
-        username: "Aamir",
-        content: "I love Data science"
-    },
-
+  {
+    id: uuidv4(),
+    username: "apna college",
+    content: "I love coding"
+  },
+  {
+    id: uuidv4(),
+    username: "kamran",
+    content: "Hard work is important"
+  }
 ];
 
 
+// 🔹 INDEX
 app.get("/posts", (req, res) => {
-    res.render("index.ejs", {posts});
-    // res.send("server working well!");
+  res.render("index.ejs", { posts });
 });
 
 
+// 🔹 NEW
 app.get("/posts/new", (req, res) => {
-    res.render("new.ejs");
+  res.render("new.ejs");
 });
 
+
+// 🔹 CREATE
 app.post("/posts", (req, res) => {
-    console.log(req.body);
-    res.send("post request working");
+  let { username, content } = req.body;
 
-    let { username, content } = req.body;
-    posts.push({ username, content });
-    res.redirect("/posts");
+  let newPost = {
+    id: uuidv4(),
+    username,
+    content
+  };
+
+  posts.push(newPost);
+
+  res.redirect("/posts");
 });
 
 
-app.post("/posts", (req, res) => {
-    let { username, content } = req.body;
-    let id = uuidv4();
-    posts.push({id, username, content });
-    res.redirect("/posts");
-    res.render("index.ejs", {posts});
-});
-
-
-
+// 🔹 SHOW
 app.get("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    console.log(id);
-let post = posts.find((p) => id === p.id);
-    res.render("show.ejs", { post });
-    
+  let { id } = req.params;
+  let post = posts.find(p => p.id === id);
+
+  if (!post) return res.status(404).send("Post not found");
+
+  res.render("show.ejs", { post });
 });
 
 
-app.patch("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    let newContent = req.body.content;
-    console.log(newContent)
-    console.log(id)
-    res.send("patch request is working ")
-    let post = posts.find((p) => id === p.id);
-    post.content = newContent;
-    console.log(post);
-    res.redirect("/posts");
-    // res.render("show.ejs", { post });
-    // res.redirect(`/posts/${id}`);
-    
-});
-
-
+// 🔹 EDIT
 app.get("/posts/:id/edit", (req, res) => {
-    let { id } = req.params;
-    let post = posts.find((p) => id === p.id);
-    res.render("edit.ejs", { post });
+  let { id } = req.params;
+  let post = posts.find(p => p.id === id);
+
+  res.render("edit.ejs", { post });
 });
 
+
+// 🔹 UPDATE (PATCH)
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find(p => p.id === id);
+
+  if (post) {
+    post.content = req.body.content;
+    res.redirect(`/posts/${id}`);
+  } else {
+    res.status(404).send("Post not found");
+  }
+});
+
+
+// 🔹 DELETE
 app.delete("/posts/:id", (req, res) => {
-    let {id} = req.params;
-    posts = posts.filter((p) => id !== p.id);
-    res.render("show.ejs", { post });
-    res.send("post deleted successfully");
-    res.redirect("/posts");
-    
+  let { id } = req.params;
+
+  posts = posts.filter(p => p.id !== id);
+
+  res.redirect("/posts");
 });
-
-
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
