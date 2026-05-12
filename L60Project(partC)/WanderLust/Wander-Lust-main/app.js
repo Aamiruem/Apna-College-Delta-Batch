d4a2f79267af92d88d80bd27e219b929ca251a6d
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-05-12 23:20:57 +0530

    commit add

--- L60Project(partC)/WanderLust/Wander-Lust-main/app.js
+++ L60Project(partC)/WanderLust/Wander-Lust-main/app.js
@@ -153,108 +153,210 @@
 
 
 
-
-// SECOND CODE 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = 8080;
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+
+const ExpressError = require("./utils/ExpressError.js");
 const User = require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = `mongodb://127.0.0.1:27017/wanderlust`;
+const port = 8080;
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+// DATABASE CONNECTION
 main()
-    .then(() => {
-        console.log(`DB CONNECTED!`);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+.then(() => {
+    console.log("DB CONNECTED!");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+
+// VIEW ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-app.use(cookieParser("signedCookie"));
 
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(methodOverride("_method"));
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(cookieParser("mysupersecretcode"));
+
+
+// SESSION CONFIG
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
+
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+
         maxAge: 7 * 24 * 60 * 60 * 1000,
+
         httpOnly: true,
     },
 };
 
-app.get("/", (req, res) => {
-    res.send(`Hii I'm Root!`);
-});
 
+// SESSION MIDDLEWARE
 app.use(session(sessionOptions));
+
+
+// FLASH
 app.use(flash());
 
+
+// PASSPORT INITIALIZE
 app.use(passport.initialize());
+
+
+// PASSPORT SESSION
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+// LOCAL STRATEGY
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+
+        try {
+
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                return done(null, false, {
+                    message: "Invalid Username",
+                });
+            }
+
+            const isMatch =
+                await user.comparePassword(password);
+
+            if (!isMatch) {
+
+                return done(null, false, {
+                    message: "Invalid Password",
+                });
+            }
+
+            return done(null, user);
+
+        } catch (err) {
+
+            return done(err);
+
+        }
+    })
+);
+
+
+// SERIALIZE USER
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+
+// DESERIALIZE USER
+passport.deserializeUser(async (id, done) => {
+
+    try {
+
+        const user = await User.findById(id);
+
+        done(null, user);
+
+    } catch (err) {
+
+        done(err);
+
+    }
+});
+
+
+// GLOBAL VARIABLES
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+
+    res.locals.success =
+        req.flash("success");
+
+    res.locals.error =
+        req.flash("error");
+
+    res.locals.currUser =
+        req.user;
+
     next();
 });
 
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Hii I'm Root!");
+});
+
+
 app.use("/listings", listingRouter);
+
 app.use("/listings/:id/reviews", reviewRouter);
+
 app.use("/", userRouter);
 
+
+// 404 HANDLER
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not Found!"));
+
+    next(
+        new ExpressError(
+            404,
+            "Page Not Found!"
+        )
+    );
 });
 
+
+// GLOBAL ERROR HANDLER
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "something went wrong!" } = err;
-    res.status(statusCode).render("error.ejs", { err });
-});
 
+    let {
+        statusCode = 500,
+        message = "Something Went Wrong!",
+    } = err;
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
+    res.status(statusCode).render("error.ejs", {
+        err,
     });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
 });
 
+
+// SERVER
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+
+    console.log(
+        `Server Running On Port ${port}`
+    );
 });
--- L60Project(partC)/WanderLust/Wander-Lust-main/models/user.js
+++ L60Project(partC)/WanderLust/Wander-Lust-main/models/user.js
@@ -1,13 +1,69 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema ({
-    email : {
-        type : String,
-        required : true
-    }
+// const userSchema = new Schema ({
+//     email : {
+//         type : String,
+//         required : true
+//     }
+// });
+
+// userSchema.plugin(passportLocalMongoose);
+// module.exports = mongoose.model('User', userSchema);
+
+
+
+
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
 });
 
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', userSchema);
+
+// HASH PASSWORD BEFORE SAVE
+userSchema.pre("save", async function(next){
+
+    if(!this.isModified("password")){
+        return next();
+    }
+
+    this.password =
+        await bcrypt.hash(this.password, 10);
+
+    next();
+});
+
+
+// COMPARE PASSWORD METHOD
+userSchema.methods.comparePassword =
+async function(password){
+
+    return await bcrypt.compare(
+        password,
+        this.password
+    );
+};
+
+
+module.exports =
+mongoose.model("User", userSchema);
