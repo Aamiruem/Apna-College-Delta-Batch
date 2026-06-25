ed67fa5f9b04448b9259bd1027ecff8d7d8157e2
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-06-25 22:48:43 +0530

    commit add

---
+++ L16JavascriptParts2/Alert.js
@@ -0,0 +1,17 @@
+alert("something is wrong");
+
+alert('Danger')
+
+console.error("this message is error")
+console.warn("this is warning")
+console.assert(false, "This assertion failed")
+
+
+
+alert()
+
+prompt()
+
+document
+
+window
---
+++ L16JavascriptParts2/tempCodeRunnerFile.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter your color: whatever you choose\n', (color) => {
+//     switch (color.toLowerCase()) {
+//         case "red":
+//             console.log("Stop");
+//             break;
+//         case "yellow":
+//             console.log("Ready");
+//             break;
+//         case "green":
+//             console.log("Go");
+//             break;
+//         default:
+//             console.log("Invalid color");
+//     }
+//     rl.close();
+// });
